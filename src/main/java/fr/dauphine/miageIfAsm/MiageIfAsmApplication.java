d5a6eb39f7d67ecdd65a3aeef43a156e495799c3
rasolohery tianome <lifakpo@gmail.com>
2018-06-25 23:14:31 +0200

    add project

---
+++ src/main/java/fr/dauphine/miageIfAsm/MiageIfAsmApplication.java
@@ -0,0 +1,12 @@
+package fr.dauphine.miageIfAsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageIfAsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiageIfAsmApplication.class, args);
+	}
+}
---
+++ src/test/java/fr/dauphine/miageIfAsm/MiageIfAsmApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.dauphine.miageIfAsm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MiageIfAsmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
